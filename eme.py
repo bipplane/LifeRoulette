7920e0aa1c4f8bf56ed7df5dfdd5cc2dfbabcc5b
bipplane <ryanchen@live.com.sg>
2024-05-31 18:23:32 +0800

    test

---
+++ eme.py
@@ -0,0 +1,5 @@
+# Key System: A content protection (DRM) mechanism.
+# Content Decryption Module (CDM): A client-side software or \
+# hardware mechanism that enables playback of encrypted media.
+# License (Key) server: Interacts with a CDM to provide keys to decrypt media.
+# Packaging service: Encodes and encrypts media for distribution/consumption.
